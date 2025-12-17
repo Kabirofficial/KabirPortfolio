88307b9cfe2673b094eee2cc17c5650ef48c15b8
Kabir Thayani <thayanikabir.official@gmail.com>
2025-12-17 15:36:14 +0530

    update img

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import logo from "/profile.webp";
+import logo from "/profile.png";
 
 const HomePage = () => {
   return (
